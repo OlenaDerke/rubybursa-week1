f933e119c7d601eab4ee0bd6f9b0bc3bfd4ccd2d
olena <dzigora.elena@gmail.com>
2015-06-20 15:42:24 +0300

    some basic ifs

---
+++ unit-1.rb
@@ -0,0 +1,17 @@
+puts "Hello World!"
+
+a = 2
+
+res = if a > 5
+	"#{a} is bigger than 5"
+elsif a < 5
+	"#{a} is smaller than 5"
+else
+	"#{a} is equal than 5"
+	"kkkkjkjkj"
+end
+
+res2 = res
+
+puts res2
+		
